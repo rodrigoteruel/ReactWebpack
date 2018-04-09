142d2c91e86c4be2550bb9f4bfc01e6992acc4d8
Rodrigo Teruel <rodrigo.teruel@hotmail.com>
2018-04-09 14:03:52 -0300

    atualização 09/04/2018 14:03

--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@
 
 import React from 'react'
 
-var Title = React.createClass({
+const App = React.createClass({
     render: function(){
-        return <h1>Título </h1>
+        return <h1>Aplicação</h1>
     }
 })
 
-export default Title
+export default App
